97262b5ca2f6a7ea404574f573a34f18748447cd
Smit R. Gadhavi <smitgadhavi205@gmail.com>
2025-04-10 17:41:30 +0530

    Add files via upload

---
+++ Hostel.py
@@ -0,0 +1,19 @@
+'''from datetime import datetime
+while 1:
+
+    dict={"Smit":1,"Vedant":2,"Shaan":3,"Sahal":4,"Neel":5,"Kavya":6,"Nikunj":7,"Divvij":8}
+    name=input("Enter your name or id or scanner")
+    if name in dict :
+        print(f"Here is your receipt {name}")
+        # Get the current date and time
+        date = datetime.now()
+
+        # Print the date and time
+        print(date)  
+        print("Message goes to parents")
+    elif name not in dict:
+        print("Try again")
+    else:
+        print("Take new card")
+    print("visit again")'''
+
